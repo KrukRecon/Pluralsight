0f7318f986568f50353fab0a696d8a41c0a2a6a0
KrukRecon <adam.kruk00@gmail.com>
2022-02-11 21:15:50 +0100

    strings

--- IntroductionToTheCS/ConsoleApp1/Program.cs
+++ IntroductionToTheCS/ConsoleApp1/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification = string.Concat(firstName, lastName);
+
+            string empId = firstName.ToLower() + "-" + lastName.ToLower();
+            string filePath = @"C:\x\d";
+
+            int x = 5;
+            int y = x;
+            x += 5;
+
+            Console.WriteLine(filePath);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
         }
     }
 }
+ 
