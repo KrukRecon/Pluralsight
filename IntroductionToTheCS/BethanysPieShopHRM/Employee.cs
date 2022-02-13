30fdfea211ba136d7ad58bddade829813ae00a12
KrukRecon <adam.kruk00@gmail.com>
2022-02-13 20:16:52 +0100

    Creating class and operating on objects

---
+++ IntroductionToTheCS/BethanysPieShopHRM/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    public class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private DateTime birthDay;
+
+        private int numberOfHoursWorked;
+        private double wage;
+        private double hourlyRate;
+        private EmployeeType employeeType;
+
+        public string FirstName 
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public Employee() { }
+        public Employee(string firstName, string lastName, string email, DateTime birthDay, EmployeeType employeeType, double hourlyRate)
+        {
+            FirstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.birthDay = birthDay;
+            this.employeeType = employeeType;
+            this.hourlyRate = hourlyRate;
+        }
+
+        public void PerformWork()
+        {
+            numberOfHoursWorked++;
+
+            Console.WriteLine($"{firstName} {lastName} is now working!");
+        }
+
+        public void StopWorking()
+        {
+            Console.WriteLine($"{firstName} {lastName} has stoped working!");
+        }
+
+        public double RecieveWage()
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"The wage for {numberOfHoursWorked} hours of work is {wage}.");
+            numberOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: {firstName},\nLast name: {lastName},\nEmail: {email},\nBirthday: {birthDay.ToShortDateString()},\nEmployee type: {employeeType},\n");
+        }
+    }
+}
---
+++ IntroductionToTheCS/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BethanysPieShopHRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(2000, 7, 10), EmployeeType.Manager, 26.97);
+            Employee george = new Employee("George", "Smith", "george@snowball.be", new DateTime(2000, 7, 10), EmployeeType.Research, 18.97);
+            Employee test = bethany;
+
+            //test.firstName = "test";
+            //test.lastName = "test";
+
+            bethany.DisplayEmployeeDetails();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.PerformWork();
+            bethany.RecieveWage();
+
+            george.DisplayEmployeeDetails();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.RecieveWage();
+        }
+    }
+}
